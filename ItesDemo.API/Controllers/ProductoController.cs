19afac9cf00bb81d66bbcd98ac0cbcc18b065a67
Federico Trani <federicotrani@gmail.com>
2023-09-05 19:02:40 -0300

    Agregar archivos de proyecto.

---
+++ ItesDemo.API/Controllers/ProductoController.cs
@@ -0,0 +1,51 @@
+﻿using ItesDemo.API.Data;
+using ItesDemo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItesDemo.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductoController : ControllerBase
+{
+    private readonly ApiDbContext context;
+
+    public ProductoController(ApiDbContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Producto>>> GetProductsAsync()
+    {
+        var products = await context.Productos.ToListAsync();
+
+        if (products != null)
+        {
+            return Ok(products);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<List<Producto>>> GetProductByIdAsync(int ID)
+    {
+        var product = await context.Productos.Where(x => x.id == ID).FirstOrDefaultAsync();
+
+        if (product != null)
+        {
+            return Ok(product);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+
+}
---
+++ ItesDemo.API/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItesDemo.API.Models;
+
+public class Usuario
+{
+    [Key]
+    public int id { get; set; }
+    public string email { get; set; }
+    public string nombre { get; set; }
+    public string password { get; set; }
+}
